ffee6b5b2ff3dd47e512a9139e14882dbcb600a1
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-01 14:01:51 +0300

    Added ввод значений с нуля

--- main.cpp
+++ main.cpp
@@ -27,11 +27,11 @@ int** createMatrix(unsigned rows, unsigned columns)
   return mat;
 }
 
-void initMatrix(int** matrix, unsigned rows, unsigned columns)
+void initMatrix(int** matrix, unsigned rows, unsigned columns, int init)
 {
   for (size_t i = 0; i < rows;i++) {
     for (size_t j = 0; j < columns; j++) {
-      matrix[i][j] = (j+1) * (i+1);
+      matrix[i][j] = init;
     }
   }
 }
@@ -39,19 +39,34 @@ void initMatrix(int** matrix, unsigned rows, unsigned columns)
 void printMatrix(int** matrix, unsigned rows, unsigned columns)
 {
   for (int i = 0; i < rows;i++) {
-    for (int j = 0; j < columns; j++) {
-      std::cout << matrix[i][j] << '\t';
+    std::cout << matrix[i][0];
+    for (int j = 1; j < columns; j++) {
+      std::cout << '\t' << matrix[i][j];
     }
     std::cout << '\n';
+  }
+}
+
+void valueByInput(int** matrix, unsigned rows, unsigned columns) {
+  int t = 0;
+  for (size_t i = 0; i < rows;i++) {
+    for (size_t j = 0; j < columns; j++) {
+      std::cin >> t;
+      matrix[i][j] = t;
+    }
   }
 }
 
 int main()
 {
   int** matrix = nullptr;
-  unsigned int rows = 0, columns = 0;
+  unsigned rows = 0, columns = 0;
 
   std::cin >> rows >> columns;
+  if (!std::cin) {
+    std::cerr << "Sudar, vse ploxo\n";
+    return 1;
+  }
 
   try {
     matrix = createMatrix(rows,columns);
@@ -60,7 +75,13 @@ int main()
     return 2;
   }
 
-  initMatrix(matrix, rows, columns);
+    valueByInput(matrix, rows, columns);
+    if (!std::cin) {
+      std::cerr << "You are mistake";
+      deleting(matrix, rows);
+      return 1;
+    }
+
   printMatrix(matrix, rows, columns);
   deleting(matrix, rows);
 
