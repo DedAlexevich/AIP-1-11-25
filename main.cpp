e9bd84e8c6e11f51d4d698a749b2e653f1f01686
DedAlexevich <kuznecovpetr502@yandex.ru>
2025-11-01 20:39:05 +0300

    Add function convert and test_convert, a little restructured code

--- main.cpp
+++ main.cpp
@@ -1,70 +1,30 @@
 #include <iostream>
-#include <thread>
 
+void deleting(int** mat, size_t created);
 
-void deleting(int** mat, size_t created)
-{
-  for (size_t i = 0; i < created; i++) {
-    delete[] mat[i];
-  }
-  delete[] mat;
-}
+int** createMatrix(size_t rows, size_t columns);
 
-int** createMatrix(unsigned rows, unsigned columns)
-{
-  int ** mat = new int*[rows];
-  size_t iter = 0;
-  try {
-    for (size_t i = 0; i < columns; i++) {
-      iter++;
-      int* c = new int[columns];
-      mat[i] = c;
-    }
-  } catch (const std::bad_alloc &e) {
-    deleting(mat, iter);
-    throw;
-  }
-  return mat;
-}
+void initMatrix(int** matrix, size_t rows, size_t columns, int init);
 
-void initMatrix(int** matrix, unsigned rows, unsigned columns, int init)
-{
-  for (size_t i = 0; i < rows;i++) {
-    for (size_t j = 0; j < columns; j++) {
-      matrix[i][j] = init;
-    }
-  }
-}
+void printMatrix(int** matrix, size_t rows, size_t columns);
+void printMatrixVarLen(int** matrix, const size_t* lns, size_t rows); //homework
 
-void printMatrix(int** matrix, unsigned rows, unsigned columns)
-{
-  for (int i = 0; i < rows;i++) {
-    std::cout << matrix[i][0];
-    for (int j = 1; j < columns; j++) {
-      std::cout << '\t' << matrix[i][j];
-    }
-    std::cout << '\n';
-  }
-}
+void valueByInput(int** matrix, size_t rows, size_t columns);
 
-void valueByInput(int** matrix, unsigned rows, unsigned columns) {
-  int t = 0;
-  for (size_t i = 0; i < rows;i++) {
-    for (size_t j = 0; j < columns; j++) {
-      std::cin >> t;
-      matrix[i][j] = t;
-    }
-  }
-}
+int** convert(const int* t, const size_t* lns, size_t rows); //homework
+void test_convert(); //homework
 
 int main()
 {
   int** matrix = nullptr;
-  unsigned rows = 0, columns = 0;
+  size_t rows = 0, columns = 0;
 
   std::cin >> rows >> columns;
   if (!std::cin) {
     std::cerr << "Sudar, vse ploxo\n";
+    return 1;
+  } else if (!rows || !columns  ){
+    std::cerr << "Error of size\n";
     return 1;
   }
 
@@ -75,7 +35,7 @@ int main()
     return 2;
   }
 
-    valueByInput(matrix, rows, columns);
+    initMatrix(matrix, rows, columns, 2);
     if (!std::cin) {
       std::cerr << "You are mistake";
       deleting(matrix, rows);
@@ -85,5 +45,109 @@ int main()
   printMatrix(matrix, rows, columns);
   deleting(matrix, rows);
 
+  test_convert();
+
   return 0;
+}
+
+void test_convert()
+{
+  int t[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
+  size_t lns[] = {4, 2, 5, 1};
+  size_t rows = 4;
+  int** matrix = nullptr;
+  try {
+    matrix = convert(t, lns, rows);
+  } catch (const std::bad_alloc &e) {
+    std::cerr << e.what();
+    throw;
+  }
+
+  printMatrixVarLen(matrix, lns, rows);
+  deleting(matrix, rows);
+}
+
+void deleting(int** mat, size_t created)
+{
+  for (size_t i = 0; i < created; i++) {
+    delete[] mat[i];
+  }
+  delete[] mat;
+}
+
+int** createMatrix(size_t rows, size_t columns)
+{
+  int** mat = new int*[rows];
+  size_t iter = 0;
+  try {
+    for (; iter < rows; iter++) {
+      int* c = new int[columns];
+      mat[iter] = c;
+    }
+  } catch (const std::bad_alloc) {
+    deleting(mat, iter);
+    throw;
+  }
+  return mat;
+}
+
+void initMatrix(int** matrix, size_t rows, size_t columns, int init)
+{
+  for (size_t i = 0; i < rows;i++) {
+    for (size_t j = 0; j < columns; j++) {
+      matrix[i][j] = init;
+    }
+  }
+}
+
+void printMatrix(int** matrix, size_t rows, size_t columns)
+{
+  for (size_t i = 0; i < rows;i++) {
+    std::cout << matrix[i][0];
+    for (size_t j = 1; j < columns; j++) {
+      std::cout << '\t' << matrix[i][j];
+    }
+    std::cout << '\n';
+  }
+}
+
+void printMatrixVarLen(int** matrix, const size_t* lns, size_t rows)
+{
+  for (size_t i = 0; i < rows;i++) {
+    std::cout << matrix[i][0];
+    for (size_t j = 1; j < lns[i]; j++) {
+      std::cout << '\t' << matrix[i][j];
+    }
+    std::cout << '\n';
+  }
+}
+
+void valueByInput(int** matrix, size_t rows, size_t columns)
+{
+  int t = 0;
+  for (size_t i = 0; i < rows;i++) {
+    for (size_t j = 0; j < columns; j++) {
+      std::cin >> t;
+      matrix[i][j] = t;
+    }
+  }
+}
+
+int** convert(const int* t, const size_t* lns, size_t rows) //homework
+{
+  int** mat = new int*[rows];
+  size_t c = 0;
+  for (size_t i = 0; i < rows; ++i) {
+    try {
+      mat[i] = new int[lns[i]];
+    } catch (const std::bad_alloc) {
+      deleting(mat, i);
+      throw;
+    }
+    for (size_t j = 0; j < lns[i]; ++j) {
+      mat[i][j] = t[c];
+      ++c;
+    }
+  }
+  return mat;
 }
